f1be2a9f7c0fcc9828588680365195e92a7c517e
Ammar <dev.ammar15@gmail.com>
2024-09-08 11:56:36 +0500

    Projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -15,51 +15,103 @@ const ProjectCard = ({
   image,
   source_code_link,
   isMobile,
-}) => (
-  <Tilt
-    className={`${
-      !isMobile && "w-full xs:w-[350px]"
-    } bg-tertiary p-5 rounded-2xl`}
-  >
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
-      <div className="relative p-[1px] rounded-[20px] shadow-card">
-        <img
-          src={image}
-          alt="project_image"
-          className="w-full h-full object-cover rounded-2xl"
-        />
+}) => {
+  if (isMobile) {
+    return (
+      <div className="border-[#242424] bg-black-200 border-[1px] p-[15px] rounded-[25px]">
+        <div className="bg-tertiary relative p-[1px] rounded-[20px] shadow-card">
+          <img
+            src={image}
+            alt="project_image"
+            className="w-full h-full object-cover rounded-2xl"
+          />
 
-        <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-          <div
-            onClick={() => window.open(source_code_link, "_blank")}
-            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-          >
-            <img
-              src={github}
-              alt="source code"
-              className="w-1/2 h-1/2 object-contain"
-            />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="source code"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="mt-5">
-        <h3 className="text-white font-bold text-center text-[24px]">{name}</h3>
-        <p className="mt-2 text-secondary text-justify text-[14px]">
-          {description}
-        </p>
-      </div>
-
-      <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag) => (
-          <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
-            #{tag.name}
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-center text-[24px]">
+            {name}
+          </h3>
+          <p className="mt-2 text-secondary text-justify text-[14px]">
+            {description}
           </p>
-        ))}
+        </div>
+
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p
+              key={`${name}-${tag.name}`}
+              className={`text-[14px] ${tag.color}`}
+            >
+              #{tag.name}
+            </p>
+          ))}
+        </div>
       </div>
-    </motion.div>
-  </Tilt>
-);
+    );
+  }
+
+  if (!isMobile) {
+    return (
+      <Tilt className="w-full xs:w-[350px] bg-tertiary p-5 rounded-2xl">
+        <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+          <div className="relative p-[1px] rounded-[20px] shadow-card">
+            <img
+              src={image}
+              alt="project_image"
+              className="w-full h-full object-cover rounded-2xl"
+            />
+
+            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+              <div
+                onClick={() => window.open(source_code_link, "_blank")}
+                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+              >
+                <img
+                  src={github}
+                  alt="source code"
+                  className="w-1/2 h-1/2 object-contain"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-5">
+            <h3 className="text-white font-bold text-center text-[24px]">
+              {name}
+            </h3>
+            <p className="mt-2 text-secondary text-justify text-[14px]">
+              {description}
+            </p>
+          </div>
+
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p
+                key={`${name}-${tag.name}`}
+                className={`text-[14px] ${tag.color}`}
+              >
+                #{tag.name}
+              </p>
+            ))}
+          </div>
+        </motion.div>
+      </Tilt>
+    );
+  }
+};
 
 const Works = () => {
   const [isMobile, setIsMobile] = useState(false);
